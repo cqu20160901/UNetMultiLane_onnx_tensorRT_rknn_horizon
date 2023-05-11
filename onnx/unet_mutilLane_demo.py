f3fc39e2cc54dfac2e27e2a53f20fe58ff8c4b82
业余程序猿 <1013429609@qq.com>
2023-05-11 19:40:18 +0800

    Update unet_mutilLane_demo.py

--- onnx/unet_mutilLane_demo.py
+++ onnx/unet_mutilLane_demo.py
@@ -76,7 +76,7 @@ def test_image(img_path):
     mask = cv2.resize(mask, (img_w, img_h))
 
     for i in range(len(lane_id)):
-        if i == 0:
+        if lane_id[i] == 0:
             continue
 
         lane_type = cls_output[lane_Id_type.index(lane_id[i])]
